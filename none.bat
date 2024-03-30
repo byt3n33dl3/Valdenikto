750203d014d2211b7ed847eaa506a954a3451524
Sulaiman <151133481+Sulaimannabdul@users.noreply.github.com>
2024-03-31 00:05:41 +0800

    Update none.bat

--- none.bat
+++ none.bat
@@ -1,4 +1,37 @@
 cout <<>
+{
+int num = (rand() % 10);
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+for(int i = 0; i<>
+{
+int num = (rand() % 10);
+SetConsoleTextAttribute(outToScreen, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+system("START CLICK.bat");
+main();
+}}
+else
+{
+while (!input.eof())
+{
+input.getline(buffer,255);
+system('ca
+cout <<>
 cout <<>
 cout <<>
 cout <<>
