a482d4c6d9b5f141d8c8a9ed765827a026ad25c3
Sulaiman <151133481+Sulaimannabdul@users.noreply.github.com>
2024-03-31 00:18:32 +0800

    Update none.bat

--- none.bat
+++ none.bat
@@ -5,6 +5,85 @@ cout <<>
 cout <<>
 cout <<>
 cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+system("START CLICK.bat");
+main();
+}}
+else
+{
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+system("START CLICK.bat");
+main();
+}}
+else
+{
+while (!input.eof())
+{
+input.getline(buffer,255);
+system('ca
+cout <<>
+system("START CLICK.bat");
+main();
+}}
+else
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+system("START CLICK.bat");
+main();
+}}
+else
+{cout <<>
+cout <<>
+cout <<>
+system("START CLICK.bat");
+main();
+}}
+else
+{
+while (!input.eof())
+{
+}}
+else
+{
+while (!input.eof())
+{
+input.getline(buffer,255);
+system('ca
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
+cout <<>
 system("START CLICK.bat");
 main();
 }}
